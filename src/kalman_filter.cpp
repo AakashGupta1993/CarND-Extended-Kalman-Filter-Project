9f81c53952f64d06d529e71a7345fc1ba6c58d46
Aakash <aakash_gupta93@yahoo.com>
2018-03-23 18:56:41 +0530

    Upload repo

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,7 @@
 #include "kalman_filter.h"
 
+#include "math.h"
+
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -21,22 +23,55 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+	
+	MatrixXd Hj = tool.CalculateJacobian(x_);
+	
+	double divisor = x_(0)*x_(0)+x_(1)*x_(1);
+	double divisor_root = pow(divisor, 0.5);
+	
+	double rho = divisor_root;
+	double phi = atan2(x_(1),x_(0));
+	double rho_dot = (x_(0)*x_(2)+x_(1)*x_(3))/divisor_root;
+	
+	VectorXd z_pred(3);
+	z_pred << rho , phi, rho_dot;
+
+	VectorXd y = z - z_pred;
+	
+	y[1] = atan2(sin(y[1]),cos(y[1]));
+	 
+	 
+	MatrixXd Ht = Hj.transpose();
+	MatrixXd S = Hj * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * Hj) * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -11,15 +11,75 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	//  * the estimation vector size should equal ground truth vector size
+	if(estimations.size() != ground_truth.size()
+			|| estimations.size() == 0){
+		cout << "Invalid estimation or ground_truth data" << endl;
+		return rmse;
+	}
+
+	//accumulate squared residuals
+	for(unsigned int i=0; i < estimations.size(); ++i){
+
+		VectorXd residual = estimations[i] - ground_truth[i];
+
+		//coefficient-wise multiplication
+		residual = residual.array()*residual.array();
+		rmse += residual;
+	}
+
+	//calculate the mean
+	rmse = rmse/estimations.size();
+
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	//return the result
+	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+
+  	MatrixXd Hj(3,4);
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	//check division by zero
+	float divisor = px*px+py*py;
+	float divisor_root = pow(divisor, 0.5);
+	float divisor_3by2 = pow(divisor,1.5);
+	
+  //check division by zero
+
+	if (fabs(divisor_3by2) < 0.0001){
+
+	  std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl;
+
+	  return Hj;
+
+	}
+	if (divisor != 0){
+	    
+	    Hj(0,0) = px/divisor_root;
+    	Hj(0,1) = py/divisor_root;
+    	Hj(0,2) = 0;
+    	Hj(0,3) = 0;
+    	Hj(1,0) = -(py/divisor);
+    	Hj(1,1) = px/divisor;
+    	Hj(1,2) = 0;
+    	Hj(1,3) = 0;
+    	Hj(2,0) = py*(vx*py-vy*px)/divisor_3by2;
+    	Hj(2,1) = px*(vy*px-vx*py)/divisor_3by2;
+    	Hj(2,2) = Hj(0,0);
+    	Hj(2,3) = Hj(0,1);
+	}
+	return Hj;
 }
